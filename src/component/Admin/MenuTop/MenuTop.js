2916f2d82c3748e6baa9de2abab159c41651d9a1
Mauricio De Oliveira <mauricio.d.oliveira1@gmail.com>
2020-08-29 08:35:29 -0300

    Added menu side

--- src/component/Admin/MenuTop/MenuTop.js
+++ src/component/Admin/MenuTop/MenuTop.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { Button } from 'antd';
-import { MenuFoldOutlined, PoweroffOutlined } from '@ant-design/icons';
+import { MenuFoldOutlined, MenuUnfoldOutlined, PoweroffOutlined } from '@ant-design/icons';
 import Logo from '../../../assets/img/png/logo-white.png';
 import './MenuTop.scss';
 
-export default function MenuTop() {
+export default function MenuTop(props) {
+    const { menuCollapsed, setMenuCollapsed } = props;
+
     return (
         <div className="menu-top">
             <div className="menu-top__left">
-                <img className="menu-top__left-logo" src={Logo} alt="Mauricio De Oliveira"/>
-                <Button type="link" onClick={()=> console.log('Click!')}>
-                    <MenuFoldOutlined />
+                <Link to={"/"}>
+                    <img className="menu-top__left-logo" src={Logo} alt="Mauricio De Oliveira"/>
+                </Link>
+                <Button type="link" onClick={() => setMenuCollapsed(!menuCollapsed)}>
+                    {menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
                 </Button>
             </div>
             
